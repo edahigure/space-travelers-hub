ab36bd7b9134591c0cf60b56d7c56e8f29dae0f4
elad237 <tumbu.elad@gmail.com>
2023-03-18 16:42:18 +0100

    Install bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'src/bootstrap/css/bootstrap.min.css';
+import 'src/bootstrap/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
